ff30c6b47885a9065d2c7dca2205ff1912523042
Laura Simon Pedroso <laura.simon.pedroso@gmail.com>
2020-03-27 14:10:04 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { ScientificComponent } from './scientific/scientific.component';
+import { TechnicalComponent } from './technical/technical.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { OebComponent } from './oeb/oeb.component';
+
+const appRoutes: Routes = [
+  { path: 'oeb', component: OebComponent},
+  { path: 'scientific', component: ScientificComponent},
+  { path: 'technical', component: TechnicalComponent},
+  { path: '', redirectTo: '/oeb', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScientificComponent,
+    TechnicalComponent,
+    PageNotFoundComponent,
+    OebComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes, { enableTracing: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
