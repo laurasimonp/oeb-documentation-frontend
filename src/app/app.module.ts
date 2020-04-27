72c75f8c6dcf0d4b5f87f97c0744b3428af14512
Laura Simon Pedroso <laura.simon.pedroso@gmail.com>
2020-04-27 14:00:07 +0200

    readme as html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TopicComponent } from './topic/topic.component';
 import { RepositoryComponent } from './repository/repository.component';
 import { RepositoryDataComponent } from './repository-data/repository-data.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MarkedPipe } from './marked.pipe';
 
 @NgModule({
   imports: [
@@ -31,7 +32,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     TopicComponent,
     RepositoryComponent,
     RepositoryDataComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MarkedPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
