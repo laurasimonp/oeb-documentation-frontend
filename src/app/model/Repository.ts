02284cb20be9ee0b167d2aedc805ed02f3bad038
Laura Simon Pedroso <laura.simon.pedroso@gmail.com>
2020-05-18 15:55:49 +0200

    contributors

--- src/app/model/Repository.ts
+++ src/app/model/Repository.ts
@@ -1,4 +1,5 @@
 import { Topic } from './Topic';
+import { Contributor } from './Contributor';
 
 export class Repository {
     private _id: string;
@@ -6,177 +7,177 @@ export class Repository {
     private _topics: string[];
     private _description: string;
     private _url: string;
-    private _contributors: string[];
+    private _contributors: Contributor[];
     private _languages: string[];
     private _license: string;
     private _readme: any;
 
 	/**
      *Creates an instance of Repository.
-     * @param {string} [id]
-     * @param {string} [name]
-     * @param {string[]} [topics]
-     * @param {string} [description]
-     * @param {string} [url]
-     * @param {string[]} [contributors]
-     * @param {string[]} [languages]
-     * @param {string} [license]
-     * @param {*} [readme]
+     * @param {string} id
+     * @param {string} name
+     * @param {string[]} topics
+     * @param {string} description
+     * @param {string} url
+     * @param {Contributor[]} contributors
+     * @param {string[]} languages
+     * @param {string} license
+     * @param {*} readme
      * @memberof Repository
      */
-    constructor(id?: string, name?: string, topics?: string[], description?: string, url?: string, contributors?: string[], languages?: string[], license?: string, readme?: any) {
-		this._id = id;
-		this._name = name;
-		this._topics = topics;
-		this._description = description;
-		this._url = url;
-		this._contributors = contributors;
-		this._languages = languages;
-		this._license = license;
-		this._readme = readme;
-	}
+    constructor(id?: string, name?: string, topics?: string[], description?: string, url?: string, contributors?: Contributor[], languages?: string[], license?: string, readme?: any) {
+        this._id = id;
+        this._name = name;
+        this._topics = topics;
+        this._description = description;
+        this._url = url;
+        this._contributors = contributors;
+        this._languages = languages;
+        this._license = license;
+        this._readme = readme;
+    }
 
     /**
      * Getter id
      * @return {string}
      */
-	public get id(): string {
-		return this._id;
-	}
+    public get id(): string {
+        return this._id;
+    }
 
     /**
      * Getter name
      * @return {string}
      */
-	public get name(): string {
-		return this._name;
-	}
+    public get name(): string {
+        return this._name;
+    }
 
     /**
      * Getter topics
      * @return {string[]}
      */
-	public get topics(): string[] {
-		return this._topics;
-	}
+    public get topics(): string[] {
+        return this._topics;
+    }
 
     /**
      * Getter description
      * @return {string}
      */
-	public get description(): string {
-		return this._description;
-	}
+    public get description(): string {
+        return this._description;
+    }
 
     /**
      * Getter url
      * @return {string}
      */
-	public get url(): string {
-		return this._url;
-	}
+    public get url(): string {
+        return this._url;
+    }
 
     /**
      * Getter contributors
-     * @return {string[]}
+     * @return {Contributor[]}
      */
-	public get contributors(): string[] {
-		return this._contributors;
-	}
+    public get contributors(): Contributor[] {
+        return this._contributors;
+    }
 
     /**
      * Getter languages
      * @return {string[]}
      */
-	public get languages(): string[] {
-		return this._languages;
-	}
+    public get languages(): string[] {
+        return this._languages;
+    }
 
     /**
      * Getter license
      * @return {string}
      */
-	public get license(): string {
-		return this._license;
-	}
+    public get license(): string {
+        return this._license;
+    }
 
     /**
      * Getter readme
      * @return {any}
      */
-	public get readme(): any {
-		return this._readme;
-	}
+    public get readme(): any {
+        return this._readme;
+    }
 
     /**
      * Setter id
      * @param {string} value
      */
-	public set id(value: string) {
-		this._id = value;
-	}
+    public set id(value: string) {
+        this._id = value;
+    }
 
     /**
      * Setter name
      * @param {string} value
      */
-	public set name(value: string) {
-		this._name = value;
-	}
+    public set name(value: string) {
+        this._name = value;
+    }
 
     /**
      * Setter topics
      * @param {string[]} value
      */
-	public set topics(value: string[]) {
-		this._topics = value;
-	}
+    public set topics(value: string[]) {
+        this._topics = value;
+    }
 
     /**
      * Setter description
      * @param {string} value
      */
-	public set description(value: string) {
-		this._description = value;
-	}
+    public set description(value: string) {
+        this._description = value;
+    }
 
     /**
      * Setter url
      * @param {string} value
      */
-	public set url(value: string) {
-		this._url = value;
-	}
+    public set url(value: string) {
+        this._url = value;
+    }
 
     /**
      * Setter contributors
-     * @param {string[]} value
+     * @param {Contributor[]} value
      */
-	public set contributors(value: string[]) {
-		this._contributors = value;
-	}
+    public set contributors(value: Contributor[]) {
+        this._contributors = value;
+    }
 
     /**
      * Setter languages
      * @param {string[]} value
      */
-	public set languages(value: string[]) {
-		this._languages = value;
-	}
+    public set languages(value: string[]) {
+        this._languages = value;
+    }
 
     /**
      * Setter license
      * @param {string} value
      */
-	public set license(value: string) {
-		this._license = value;
-	}
+    public set license(value: string) {
+        this._license = value;
+    }
 
     /**
      * Setter readme
      * @param {any} value
      */
-	public set readme(value: any) {
-		this._readme = value;
-	}
+    public set readme(value: any) {
+        this._readme = value;
+    }
 }
--- src/app/repository-data/repository-data.component.ts
+++ src/app/repository-data/repository-data.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Repository } from '../model/Repository';
-import { Contributor } from '../model/Contributor';
 
 @Component({
   selector: 'app-repository-data',
@@ -13,17 +12,13 @@ export class RepositoryDataComponent implements OnInit {
 
   //attributes
   @Input() repo: Repository;
-  conts: Contributor;
-
+  
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     if (!this.repo) {
       this.http.get<Repository>('http://localhost:8080/github-docs-backend/repository/benchmarking-data-model').subscribe(data => {
         this.repo = data;
-      });
-      this.http.get<Contributor>('http://localhost:8080/github-docs-backend/contributors/benchmarking-data-model').subscribe(data => {
-        this.conts = data;
       });
     }
   }
